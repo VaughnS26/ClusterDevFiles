ee813a2a29d88e8ccb8c5595745680c37d3000a5
Grant Matteo <matteoboy3@gmail.com>
2023-01-20 00:07:27 -0500

    Add files via upload

---
+++ Configure-SMB.ps1
@@ -0,0 +1,19 @@
+﻿function Configure-SMB{
+    
+    param(
+        [string] $name = "default",
+        [string] $path = "C:\"
+        )
+
+    $Parameters = @{
+    Name = $name
+    Path = $path
+    FullAccess = "Everyone"
+
+    }
+    if (!(Test-Path $path -PathType Container)) {
+    New-Item -ItemType Directory -Force -Path $path
+}
+
+    New-SmbShare @Parameters
+}
