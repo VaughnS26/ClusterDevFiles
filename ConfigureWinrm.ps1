5910d5d08267c033bda894db3bc10f31f96c4a6b
Grant Matteo <matteoboy3@gmail.com>
2023-01-21 19:33:02 -0500

    Update ConfigureWinrm.ps1

--- ConfigureWinrm.ps1
+++ ConfigureWinrm.ps1
@@ -8,7 +8,7 @@ function Configure-Winrm{
         $hostName = $env:COMPUTERNAME
         $hostIP = (Get-NetAdapter | Get-NetIPAddress).IPv4Address | Out-String
         $cert = New-SelfSignedCertificate -DnsName $hostName,$hostIP -CertStoreLocation "cert:\LocalMachine\My"
-        New-Item -Path WSMan:\localhost\Listener -Transport HTTTPS -Address * -CertificateThumbPrint $cert.Thumbprint -Force
+        New-Item -Path WSMan:\localhost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $cert.Thumbprint -Force
         New-NetFirewallRule -DisplayName 'winrm' -Name 'winrm' -Profile Any -LocalPort 5986 -Protocol TCP -Action Allow
         #Restart-Service WinRM
         #While ((Get-Service WinRM).Status -ne "Running") {
