f6f97f8907d0bf49c32b2a9792bb320e1687ef51
Grant Matteo <matteoboy3@gmail.com>
2023-01-21 20:07:35 -0500

    Update ConfigureIIS.ps1

--- ConfigureIIS.ps1
+++ ConfigureIIS.ps1
@@ -11,7 +11,7 @@
         $hostName = $env:COMPUTERNAME
         $hostIP = (Get-NetAdapter | Get-NetIPAddress).IPv4Address | Out-String
         $cert = New-SelfSignedCertificate -DnsName $hostName,$hostIP -CertStoreLocation "cert:\LocalMachine\My"
-        New-WebBinding -Name "Default Web Server" -IP "*" -Port 443 -Protocol https
+        New-WebBinding -Name "Default Web Site" -IP "*" -Port 443 -Protocol https
         cd IIS:\SslBindings\
         get-item Cert:\LocalMachine\My\$cert.thumbprint | new-item 0.0.0.0!443
 
